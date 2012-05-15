564bec4a6a2de5e80d16db8225484f4259801efe
Davide Principi <davide.principi@nethesis.it>
2012-05-15 09:01:37 +0200

    Samba.php SharedFolder Plugin Template: changed field order. Refs #1090 - SharedFolder UI module: implement plugin structure

--- root/usr/share/nethesis/NethServer/Template/SharedFolder/Plugin/Samba.php
+++ root/usr/share/nethesis/NethServer/Template/SharedFolder/Plugin/Samba.php
@@ -1,14 +1,17 @@
 <?php
+
 $baseTabColumnSet1 = $view->columns();
 
 $baseTabColumnSet1->insert($view->fieldset()->setAttribute('template', $T('Recycle'))
-        ->insert($view->fieldsetSwitch('RecycleBin', 'enabled')
+        ->insert($view->radioButton('RecycleBin', 'disabled'))
+        ->insert($view->fieldsetSwitch('RecycleBin', 'enabled', $view::FIELDSETSWITCH_EXPANDABLE)
             ->insert($view->checkBox('KeepVersions', 'enabled')->setAttribute('uncheckedValue', 'disabled'))
         )
-        ->insert($view->fieldsetSwitch('RecycleBin', 'disabled')));
+);
 
 $baseTabColumnSet1->insert($view->fieldset()->setAttribute('template', $T('Shadow copy'))
-        ->insert($view->fieldsetSwitch('ShadowCopy', 'enabled'))
-        ->insert($view->fieldsetSwitch('ShadowCopy', 'disabled')));
+        ->insert($view->radioButton('ShadowCopy', 'disabled'))
+        ->insert($view->radioButton('ShadowCopy', 'enabled'))
+);
 
 echo $baseTabColumnSet1;
