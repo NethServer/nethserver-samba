6216bc9e039dd53f4e4da4dda6fcac92e1a995f5
Davide Principi <davide.principi@nethesis.it>
2013-04-17 16:16:26 +0200

    Samba SharedFolder UI plugin: hide profile widget. Refs #1881

--- root/usr/share/nethesis/NethServer/Template/SharedFolder/Plugin/Samba.php
+++ root/usr/share/nethesis/NethServer/Template/SharedFolder/Plugin/Samba.php
@@ -11,15 +11,15 @@ $vfsRecycle = $view->fieldsetSwitch('SmbRecycleBinStatus', 'enabled', $view::FIE
 
 # $vfsShadow = $view->checkBox('SmbShadowCopyStatus', 'enabled')->setAttribute('uncheckedValue', 'disabled');
 
-$customValues = $view->fieldset()->setAttribute('template', $T('Profiles_label'))
-    ->insert($view->radioButton('profileName', 'default'))
-    ->insert($view->fieldsetSwitch('profileName', 'custom', $view::FIELDSETSWITCH_EXPANDABLE )
-        ->insert($view->textInput('customValue', $view::LABEL_NONE)))
-    ;
+//$customValues = $view->fieldset()->setAttribute('template', $T('Profiles_label'))
+//    ->insert($view->radioButton('profileName', 'default'))
+//    ->insert($view->fieldsetSwitch('profileName', 'custom', $view::FIELDSETSWITCH_EXPANDABLE )
+//        ->insert($view->textInput('customValue', $view::LABEL_NONE)))
+//    ;
 
 echo $view->fieldsetSwitch('SmbStatus', 'enabled', $view::FIELDSETSWITCH_CHECKBOX)
     ->setAttribute('uncheckedValue', 'disabled')
     ->insert($vfsRecycle)
 #    ->insert($vfsShadow)
-    ->insert($customValues)
+//    ->insert($customValues)
 ;
