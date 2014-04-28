2366de57c977bc78d4b5549b083bb59c6e00927e
Ana Fernandez <anuska_716@hotmail.com>
2014-04-28 10:24:21 +0200

    Web interface, inline help: add Spanish translation. Refs #2732"

---
+++ root/usr/share/nethesis/NethServer/Language/es/NethServer_Module_Workgroup.php
@@ -0,0 +1,18 @@
+<?php 
+
+$L['AdsController_label'] = 'Controlador';
+$L['AdsRealm_label'] = 'Realm';
+$L['login_label'] = 'Nombre de usuario';
+$L['RoamingProfiles_label'] = 'Habilitar perfiles móviles'; 
+$L['ServerRole_ADS_label'] = 'Miembro de Active Directory';
+$L['ServerRole_PDC_label'] = 'Controlador primario del dominio';
+$L['ServerRole_WS_label'] = 'Puesto de trabajo';
+$L['AdsDomain_label'] = 'Dominio';
+$L['PdcDomain_label'] = 'Dominio';
+$L['WinregistryPatches_label'] = 'Configuración del registro del cliente';
+$L['Configure_header'] = 'Función de servidor de red de Windows';
+$L['Authenticate_header'] = 'Credenciales de administrador de Active Directory';
+$L['Workgroup_Description'] = 'Administrar la red de Windows (SAMBA)';
+$L['Workgroup_Tags'] = 'samba pdc ventana de  puesto de trabajo grupo de trabajo  pdc controlador de domino compartido de itinerancia realm smb';
+$L['Workgroup_Title'] = 'Red de Windows';
+$L['Join_failed_message'] = 'Error al unirse a Active Directory';
